28b378da667297d53f9617682495aa3a19511009
Even Pan <even@fareastone.com.tw>
2022-08-08 01:28:04 +0800

    add startup and shutdown commend for app

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
-from app.api import ping
 
+from app.api import ping
+from app.db import engine, database, metadata
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
 
 app.include_router(ping.router)
