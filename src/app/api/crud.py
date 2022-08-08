29117ed16f7e290704f64784ba0b5c38363a22cc
Even Pan <even@fareastone.com.tw>
2022-08-08 22:58:00 +0800

    add feature for the route

--- src/app/api/crud.py
+++ src/app/api/crud.py
@@ -4,3 +4,7 @@ from app.db import notes, database
 async def post(payload: NoteSchema):
     query = notes.insert().values(title=payload.title, description=payload.description)
     return await database.execute(query=query)
+
+async def get(id: int):
+    query = notes.select().where(id == notes.c.id)
+    return await database.fetch_one(query=query)
