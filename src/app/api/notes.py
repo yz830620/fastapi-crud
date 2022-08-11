6b48fbf8fe3828132db741391d187cc2487039bb
Even Pan <even@fareastone.com.tw>
2022-08-12 00:50:42 +0800

    test driven put route

--- src/app/api/notes.py
+++ src/app/api/notes.py
@@ -43,3 +43,25 @@ async def read_note(id: int):
 async def read_all_note():
     """route for read all notes"""
     return await crud.get_all()
+
+@router.put("/{id}/", response_model=NoteDB)
+async def update_note(id: int, payload: NoteSchema):
+  """route for update the note
+    - Parameters:
+      - id: int, id to update
+      - payload: NoteSchema, updated info
+    - Return:
+      - response_object: NoteDB
+  """
+  note = await crud.get(id)
+  if not note:
+    raise HTTPException(status_code=404, detail="Note not found")
+
+  note_id = await crud.put(id, payload)
+
+  response_object = {
+    "id": note_id,
+    "title": payload.title,
+    "description": payload.description
+  }
+  return response_object
